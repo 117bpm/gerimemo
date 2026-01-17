fdaa6a0c14fe80f3733e04c83a9be719ab860e5c
117bpm <gisrevido0701@gmail.com>
2026-01-17 18:31:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener("fetch", () => {});
+const CACHE_NAME = 'memo-app-cache-v1';
+const urlsToCache = [
+  '/memo.html',
+  '/icon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
